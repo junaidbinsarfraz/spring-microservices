26e94b00b475a879d69b226899ce614e819c7adb
Junaid <junaidbinsarfraz@yahoo.com>
2022-06-07 16:04:18 +0500

    Hystrix properties configured

--- movie-catalog-service/src/main/java/com/movie/moviecatalogservice/services/CatalogItemService.java
+++ movie-catalog-service/src/main/java/com/movie/moviecatalogservice/services/CatalogItemService.java
@@ -4,6 +4,7 @@ import com.movie.moviecatalogservice.models.CatalogItem;
 import com.movie.moviecatalogservice.models.Movie;
 import com.movie.moviecatalogservice.models.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +15,14 @@ public class CatalogItemService {
     @Autowired
     private RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    @HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "50"),
+                    @HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "5000"),
+            }
+    )
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject("http://MOVIE-INFO-SERVICE/movies/" + rating.getMovieId(), Movie.class);
         return new CatalogItem(movie.getName(), "Movie Desc", rating.getRating());
